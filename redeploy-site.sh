e5f7e94f36025f3c51633c15f76b9b1f8c1bcbc2
Anthony Holder <AnthonyH202x@gmail.com>
2022-06-19 07:31:37 -0400

    push redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 pkill -f tmux
-cd project-h-squad-AnthonyH
+cd project-h-squad-AnthonyH
 git fetch && git reset origin/main --hard
-pip3 install -r requirements.txt
-tmux new -d -s pfolio-session
-cd project-h-squad-AnthonyH
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
-export FLASK_ENV=development
-flask run 
+pip3 install -r requirements.txt
+tmux new -d -s "portfolio-project" flask run --host=0.0.0.0  
