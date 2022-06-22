12b61b3920708ef03a2ddf24ca2919b9bb43c52e
Anthony Holder <AnthonyH202x@gmail.com>
2022-06-22 11:11:12 -0400

    created .env

--- app/__init__.py
+++ app/__init__.py
@@ -36,7 +36,7 @@ def education():
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
 print(mydb)
 
-class TimelinePost():
+class TimelinePost(Model):
     name = CharField()
     email = CharField()
     content = TextField()
