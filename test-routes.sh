013568ed62bcd5df9e1157538d1f122f15a1afb8
Anthony Holder <AnthonyH202x@gmail.com>
2022-06-22 11:13:53 -0400

    updaate test.routes.sh

--- test-routes.sh
+++ test-routes.sh
@@ -1,6 +1,5 @@
-#!bin/bash
+#!/bin/bash
 
 curl --request GET http://localhost:5000/api/timeline_post
 
-curl --request POST http://localhost:5000/api/timeline_post -d
-'name=Anthony&email=anthonyh@gmail.com&content=Just Added Dtatbase to my portfolio site'
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Anthony&email=anthonyh@gmail.com&content=Just Added Dtatbase to my portfolio site'
