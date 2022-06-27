4b1d8fc2e2753de47b09dc333563809b69481ed4
Anthony Holder <AnthonyH202x@gmail.com>
2022-06-26 23:23:24 -0400

    updated redeploy.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,6 @@
 
 cd project-h-squad-AnthonyH
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-systemctl daemon-reload
-systemctl enable myportfolio
-systemctl start myportfolio
+systemctl restart myportfolio
