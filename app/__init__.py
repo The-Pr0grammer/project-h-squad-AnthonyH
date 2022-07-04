b692ae90bcf7bba71913630396dddc7781a1a6ef
rysonw <rysonplays@gmail.com>
2022-07-03 14:14:02 -1000

    Added tests

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,12 @@ from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+    print(mydb)
 
 @app.route('/')
 def index():
@@ -37,8 +43,6 @@ def education():
 def timeline():
     return render_template('timeline.html', title='Timeline')
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),user=os.getenv("MYSQL_USER"), password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
-print(mydb)
 
 class TimelinePost(Model):
     date = CharField()
@@ -54,17 +58,32 @@ mydb.create_tables([TimelinePost])
 @app.route("/api/timeline_post", methods=["GET", "POST", "DELETE"])
 def timeline_post():
     if request.method == "POST":
-        date = request.form["date"]
-        title = request.form["title"]
-        events = request.form["events"]
+        try:
+            date = request.form['date']
+        except KeyError:
+            return "Invalid Date", 400
+
+        try:
+            title = request.form['title']
+        except KeyError:
+            return "Invalid Title", 400
+
+        try:
+            events = request.form['events']
+            if len(events) < 1:
+                return "Invalid Body", 400
+        except KeyError:
+            return "Invalid Body", 400
 
         timeline_post = TimelinePost.create(date=date,
                                             title=title,
                                             events=events)
 
         return model_to_dict(timeline_post)
-    elif request.method == "GET":
+
+    if request.method == "GET":
         return {'posts': [model_to_dict(p) for p in TimelinePost.select()]}
+
     elif request.method == "DELETE":
         del_id = request.form["id"]
         TimelinePost.delete_by_id(del_id)
