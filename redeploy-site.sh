e81e7503385c1cca8c5c0176123b6ec725671063
Anthony Holder <AnthonyH202x@gmail.com>
2022-07-19 17:01:44 -0400

    updated script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd project-h-squad-AnthonyH
+cd  "~/project-h-squad-AnthonyH"
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip3 install -r requirements.txt
+
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up --build
